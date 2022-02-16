25a5a4841101812697958c9122e738122832c081
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2022-02-16 17:59:29 +0100

    add created

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -9,11 +9,21 @@
                     <div class="card-header">Crea un nuovo Post</h2></div>
 
                     <div class="card-body"> 
-                        <form>
+                        <form action="{{route("posts.store")}}" method="POST">
+                            @csrf
                             <div class="form-group">
-                                <label for=""></label>
-                                <input type="text" name="title" id="title">
+                                <label for="title">Title</label>
+                                <input type="text" class="form-control" name="title" id="title" placeholder="Add title" >
                             </div>
+                            <div class="form-group">
+                                <label for="content">Content</label>
+                                <textarea type="text" class="form-control" name="content" id="content" placeholder="Add text" rows="8"></textarea>
+                            </div>
+                            <div class="form-group form-check">
+                                <input type="checkbox" class="form-check-input" id="published">
+                                <label class="form-check-label" for="published">Published</label>
+                            </div>
+
                             <button type="submit" class="btn btn-outline-dark">Crea</button>
                         </form>
                     </div>
